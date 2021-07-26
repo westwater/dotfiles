a6d0119b1936309dcf3cfb133c8eecffc440ef73
westwater <gmwestwater@hotmail.co.uk>
2021-07-26 15:59:13 +0100

    Added rbenv to init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,2 @@
 command -v diff-so-fancy > /dev/null || { echo "> installing diff-so-fancy"; brew install diff-so-fancy; }
+command -v rbenv > /dev/null || { echo "> installing rbenv"; brew install rbenv; }
