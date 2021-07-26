f07fd0cb69223172086fc246eb94de01cae91b1e
westwater <gmwestwater@hotmail.co.uk>
2021-07-26 16:12:07 +0100

    Added P10K to init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
+
+# Install terminal commands
 command -v diff-so-fancy > /dev/null || { echo "> installing diff-so-fancy"; brew install diff-so-fancy; }
 command -v rbenv > /dev/null || { echo "> installing rbenv"; brew install rbenv; }
 command -v direnv > /dev/null || { echo "> installing direnv"; brew install direnv; }
+
+# Install terminal theme
+[ -d $HOME/.oh-my-zsh/custom/themes/powerlevel10k ] || { echo "> installing powerlevel10k"; git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k; }
