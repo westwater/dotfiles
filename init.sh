98f5abb8b22ea393ad5a881c835fd617ad89a927
westwater <gmwestwater@hotmail.co.uk>
2021-07-26 16:02:56 +0100

    Added direnv to init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
 command -v diff-so-fancy > /dev/null || { echo "> installing diff-so-fancy"; brew install diff-so-fancy; }
 command -v rbenv > /dev/null || { echo "> installing rbenv"; brew install rbenv; }
+command -v direnv > /dev/null || { echo "> installing direnv"; brew install direnv; }
