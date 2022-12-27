4f6953fc276ce4ddf178ef385a64916d5eb54613
westwater <gmwestwater@hotmail.co.uk>
2022-12-27 21:45:52 +0000

    Fixed conditional in brew script

--- brew.sh
+++ brew.sh
@@ -3,7 +3,12 @@
 set -eo pipefail
 
 # Install brew (there is an m1 version but you need to specify arch before brew everytime)
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# Then add the following to zshrc
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/westwater/.zshrc
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if command -v brew &> /dev/null
+then
+	echo "brew already installed"
+else
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	# Then add the following to zshrc
+	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/westwater/.zshrc
+	eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
