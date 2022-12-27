338eaade11f6b2396318d538e02645e680ca6015
westwater <gmwestwater@hotmail.co.uk>
2022-12-27 21:40:29 +0000

    Updated brew install script to use bash installer

--- brew.sh
+++ brew.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 # Install brew (there is an m1 version but you need to specify arch before brew everytime)
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
  Then add the following to zshrc
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/westwater/.zshrc
 eval "$(/opt/homebrew/bin/brew shellenv)"
